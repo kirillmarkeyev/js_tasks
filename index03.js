ebf99072799bd29fdb6a929f4818031c100ce945
Кирилл Маркеев <KAlMarkeev@sberbank.ru>
2023-02-17 17:16:59 +0300

    Add index03.js

---
+++ index03.js
@@ -0,0 +1,22 @@
+/*
+Найти сумму элементов многомерного массива (любым способом)
+*/
+
+const array1 = [1, 2, [2, 5, 3, [32, 5], 3], 7, 4, 1, [1, [1, 2, [1, 2]]]];
+
+const getSumOfNestedArray = (array) => {
+  let result = 0;
+  const innerSum = (arr) => {
+    arr.forEach((el) => {
+      if (!Array.isArray(el)) {
+        result += el;
+      } else {
+        innerSum(el); // используем рекурсию для вложенных массивов
+      }
+    });
+    return result;
+  };
+  return innerSum(array);
+};
+
+console.log(getSumOfNestedArray(array1));
